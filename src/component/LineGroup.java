77e6c27b960ea70d754994b1f6933d52f6b86992
skydreamer <skydreamer210@gmail.com>
2024-03-08 17:50:52 +0900

    fix: 클래스 수정에 따른 LineGroup 수정

--- src/component/LineGroup.java
+++ src/component/LineGroup.java
@@ -1,6 +1,12 @@
 package component;
 
+import java.util.List;
+
 public class LineGroup implements Generator {
+    private int iteration;
+    private List<IntegerLine> integerLines;
+
+
     @Override
     public String generate() {
         return null;
